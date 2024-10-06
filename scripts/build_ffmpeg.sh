f75f2dfa436cf9e067b23f78efa65c1c8766ea3e
lifegpc <g1710431395@gmail.com>
2024-10-06 04:41:00 +0000

    Fix

--- scripts/build_ffmpeg.sh
+++ scripts/build_ffmpeg.sh
@@ -1,5 +1,5 @@
 export PREFIX=`pwd`/clib
 mkdir -p cbuild && cd cbuild || exit 1
-git clone --depth 1 'https://github.com/FFmpeg/FFmpeg' && cd ffmpeg || exit 1
+git clone --depth 1 'https://github.com/FFmpeg/FFmpeg' ffmpeg && cd ffmpeg || exit 1
 ./configure "--prefix=${PREFIX}" --enable-shared --disable-static --enable-gpl --enable-version3 --disable-doc --enable-libx264 || exit 1
 make -j8 && make install || exit 1
